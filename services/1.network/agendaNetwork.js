42c43bd4da072587a310a0f9f2310c3f5ba19edb
Jorge <jorgearojas25@gmail.com>
2021-10-12 03:13:29 -0500

    change request controller action

--- services/1.network/agendaNetwork.js
+++ services/1.network/agendaNetwork.js
@@ -17,7 +17,7 @@ router.get("/", (req, res) => {
 
 router.post("/filter", (req, res) => {
   controller
-    .AddAgenda(req.body)
+    .GetAgenda(req.body)
     .then((data) => {
       response.success(req, res, data, 201);
     })
