1942f152afe6c5655e578412d1ef4dbdfb582242
Jorge <jorgearojas25@gmail.com>
2021-10-12 12:14:55 -0500

    update agenda util

--- services/BOs/agendaBarrio.js
+++ services/BOs/agendaBarrio.js
@@ -2,6 +2,8 @@ class Agenda {
   constructor(data) {
     this.Id_usuario = data.Id_usuario;
     this.Id_doctor = data.Id_doctor;
+    this.Comentarios = data.Comentarios;
+    this.Finalizada = data.Finalizada;
     this.Fecha = data.Fecha;
     this.Hora = data.Hora;
     this.Bloqueado = data.Bloqueado;
--- services/Models/agendaModel.js
+++ services/Models/agendaModel.js
@@ -5,6 +5,8 @@ const mySchema = new Schema({
   Id_usuario: { type: Schema.ObjectId, ref: "Usuario" },
   Id_doctor: { type: Schema.ObjectId, ref: "Usuario" },
   Fecha: { type: String, required: true },
+  Comentarios: [{ isUser: Boolean, comment: String }],
+  Finalizada: { type: Boolean, required: true, default: false },
   Hora: { type: String, required: true },
   Bloqueado: { type: Boolean, require: true, default: false },
   Llamada: { type: String, require: true, default: "" },
